e8783e71d86b71354ddcb66f6afa0f9fdae01554
Jason Benson <jasonbenson3rd@gmail.com>
2018-11-13 12:07:50 -0500

    first commit

---
+++ python.101.py
@@ -0,0 +1,13 @@
+print "Hello, World";
+print ("Hello, World");
+
+print """
+    It was a dark and stormy night.
+        A murder happened.
+"""
+
+# this is a comment 
+
+theBestClass = "The 11-18 immersive"
+print theBestClass
+
