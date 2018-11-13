5f5bd99de808b4a7f4b4111ebcf923906a86b90b
jasonpbenson <jasonbenson3rd@gmail.com>
2018-11-13 14:28:19 -0500

    Add operators

--- python.101.py
+++ python.101.py
@@ -1,27 +1,54 @@
-print "Hello, World";
-print ("Hello, World");
+# print "Hello, World";
+# print ("Hello, World");
 
-print """
-    It was a dark and stormy night.
-        A murder happened.
-"""
+# print """
+#     It was a dark and stormy night.
+#         A murder happened.
+# """
 
-# this is a comment 
+# # this is a comment 
 
-theBestClass = "The 11-18 immersive"
-print theBestClass
+# theBestClass = "The 11-18 immersive"
+# print theBestClass
 
-month = "November"
-print type(month)
-date = 13
-print type(date)
-dateAsFloat = 13.0
-print type(dateAsFloat)
-aBool = True 
-print type(aBool)
+# month = "November"
+# print type(month)
+# date = 13
+# print type(date)
+# dateAsFloat = 13.0
+# print type(dateAsFloat)
+# aBool = True 
+# print type(aBool)
 
-aList = []
-print type(aList)
-aDictionary = {}
-print type(aDictionary)
+# aList = []
+# print type(aList)
+# aDictionary = {}
+# print type(aDictionary)
 
+# first = "Jason"
+# last = "Benson"
+# fullName = first + last
+# print fullName
+# fullName = first + " " + last
+# print fullName
+
+# fourteen = 10 + 4
+# print fourteen
+# fourteen = "10" + "4"
+# print fourteen
+
+# #cast = change variable to new data type
+
+# fourteen = int("10") + 4
+# print fourteen
+
+
+print 2+2
+print 2-2
+print 2/2
+print 2*2
+print 2%2
+print 2%3
+print 2**3
+print 10**87
+print "_" * 20
